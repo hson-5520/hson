d9103dc95c09c86ec3b2ec63f158225afb44be6a
aakashjajoo1 <67799841+aakashjajoo1@users.noreply.github.com>
2022-12-04 18:39:45 -0500

    Changes

--- src/HSON.hs
+++ src/HSON.hs
@@ -1,7 +1,9 @@
 module HSON (HSON, Key, Value (Boolean, Number, String, Array, Object, Null, Integer), hsonEmpty, hsonSchool, hsonDog, hsonArray, hsonSingle) where
 
+import Control.Monad qualified as Monad
 import Data.Map
 import Data.Map qualified as Map
+import GHC.Arr (Array)
 import Parser qualified as P
 import Test.QuickCheck
 
@@ -23,13 +25,45 @@ type HSON = [(Key, Value)]
 
 ------------------------- HSON Generator  ----------------------------------
 
+genValue :: Gen Value
+genValue =
+  frequency
+    [ (8, fmap String (arbitrary :: Gen String)),
+      (4, fmap Integer (arbitrary :: Gen Int)),
+      (4, fmap Number (arbitrary :: Gen Double)),
+      (3, fmap Boolean (arbitrary :: Gen Bool)),
+      (2, fmap Array (genList :: Gen [Value])),
+      (1, fmap Object genHSON),
+      (1, return Null)
+    ]
+
+instance Arbitrary Value where
+  arbitrary = genValue
+  shrink (Object (x : xs)) = Object <$> ([x] : [xs])
+  shrink (Array (x : xs)) = Array <$> ([x] : [xs])
+  shrink x = []
+
+genList :: forall a. (Arbitrary a) => Gen [a]
+genList = sized gen
+  where
+    gen :: Int -> Gen [a]
+    gen n =
+      frequency
+        [ (1, return []),
+          (n, Monad.liftM2 (:) arbitrary (gen (n `div` 2)))
+        ]
+
+-- >>> sample' genValue
+-- [String "",Array [],Integer 2,Object [("\b/",Integer (-5)),("q\GSp/\t",String "ltf")],Object [("uDq=e\RS<0",Null),("`\USk",Null)],Integer 0,String "Cy\133737K<;\54426\&5%,",Number 5.811902573451713,String "\ESCQ)A\1097422N\SOH\1078443t\f0",Boolean True,String "V\1111787>[\60881I\153675i\43596\77855y\US\NUL\1076314V\ESC"]
+
 genHSON :: Gen HSON
-genHSON = undefined
+genHSON = genList :: Gen [(Key, Value)]
 
 instance Arbitrary HSON where
   arbitrary = genHSON
 
-  shrink x = undefined
+  shrink (x : xs) = [x] : [xs]
+  shrink [] = []
 
 ---------------------------- Sample HSON ---------------------------------------
 hsonEmpty :: HSON
--- test/HSONTest.hs
+++ test/HSONTest.hs
@@ -1,28 +1,27 @@
 module HSONTest where
 
 import Control.Applicative
-import Data.Map qualified as Map
 import FromJSON
 import HSON (HSON, Key, Value (Array, Boolean, Null, Number, Object, String), hsonArray, hsonDog, hsonEmpty, hsonSchool, hsonSingle)
 import Lib
 import Parser
-import Parser qualified as P
 import Test.HUnit (Test (TestList), assert, runTestTT, (~:), (~?=))
 import Test.QuickCheck
 import ToJSON
 
 ---------------------------- QuickCheck: toJSON fromJSON  ---------------------------------------
 
--- >>> quickCheck prop_toAndFrom
+-- >>> quickCheck prop_roundtrip
 
--- prop_toAndFrom :: HSON -> Property
--- prop_toAndFrom x =
---   let y = helper x
---    in property (y == x)
---   where
---     helper x = do
---       toJSON "HSONTestFile.txt" x
---       z <- parseJSON "HSONTestFile.txt"
---       case z of
---         Right x -> return x
---         Left y -> return Empty
+prop_roundtrip :: HSON -> Bool
+prop_roundtrip hson =
+  case helper hson of
+    Nothing -> False
+    Just y -> y == hson
+  where
+    helper :: HSON -> Maybe HSON
+    helper x =
+      let y = hsonToString hson
+       in case parse hsonP y of
+            Left _ -> Nothing
+            Right x -> Just x
