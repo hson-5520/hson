d4d9459d921584285b1639d8185e676d495d3a5b
Yathu Nadanapathan <69991841+yathu-n@users.noreply.github.com>
2022-12-04 19:02:24 -0500

    QC

---
+++ src/HSONQuickCheck.hs
@@ -0,0 +1,27 @@
+module HSONQuickCheck where
+
+import Control.Applicative
+import FromJSON
+import HSON (HSON, Key, Value (Array, Boolean, Null, Number, Object, String), hsonArray, hsonDog, hsonEmpty, hsonSchool, hsonSingle)
+import Lib
+import Parser
+import Test.HUnit (Test (TestList), assert, runTestTT, (~:), (~?=))
+import Test.QuickCheck
+import ToJSON
+
+---------------------------- QuickCheck: toJSON fromJSON  ---------------------------------------
+
+-- >>> quickCheck prop_roundtrip
+
+prop_roundtrip :: HSON -> Bool
+prop_roundtrip hson =
+  case helper hson of
+    Nothing -> False
+    Just y -> y == hson
+  where
+    helper :: HSON -> Maybe HSON
+    helper x =
+      let y = hsonToString hson
+       in case parse hsonP y of
+            Left _ -> Nothing
+            Right x -> Just x
